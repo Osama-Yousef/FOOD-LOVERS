ce688ae29abd19e0702dc9c712d04d693c3b3b1c
Osama Yousef <60566900+Osama-Yousef@users.noreply.github.com>
2020-02-05 23:53:28 +0200

    Add files via upload

---
+++ add-content.js
@@ -0,0 +1,9 @@
+var name=prompt("WELCOME ! What's your name?");
+alert("Really Nice to meet you in my page!");
+var food=confirm ("Do you love food ?");
+if (food==true) {
+document.write ("Hope you enjoy my webpage!");
+}
+else {document.write (" Okay , thanks for visiting my site!");
+}
+document.write(`<h3>` + greeting + `<h3>`); 
